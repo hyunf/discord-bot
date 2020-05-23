296d0a5a93ef542c14d7fda1f1795c9090e42e85
hands8142 <46277532+hands8142@users.noreply.github.com>
2020-05-23 14:34:32 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 import discord
 import os
-import datetime
 from configparser import ConfigParser
 from discord.ext import commands, tasks
 from itertools import cycle
 
+
+
 config = ConfigParser()
 config.read('config.ini')
 prefix = config["setting"]["prefix"]
+owners = config["setting"]["owners"]
 
 
 
@@ -20,10 +22,42 @@ client.remove_command('help')
 
 
 
+@client.command(name="로드")
+async def load(ctx, extension):
+    if not ctx.author.id == int(owners):
+        return
+    else:
+        client.load_extension(f'cogs.{extension}')
+        await ctx.send(f":white_check_mark: {extension}을(를) 로드했습니다!")
+
+@client.command(name="언로드")
+async def unload(ctx, extension):
+    if not ctx.author.id == int(owners):
+        return
+    else:
+        client.unload_extension(f'cogs.{extension}')
+        await ctx.send(f":white_check_mark: {extension}을(를) 언로드했습니다!")
+
+@client.command(name="리로드")
+async def reload_commands(ctx, extension=None):
+    if not ctx.author.id == int(owners):
+        return
+    else:
+        if extension is None: # extension이 None이면 (그냥 !리로드 라고 썼을 때)
+            for filename in os.listdir("cogs"):
+                if filename.endswith(".py"):
+                    client.unload_extension(f"cogs.{filename[:-3]}")
+                    client.load_extension(f"cogs.{filename[:-3]}")
+                    await ctx.send(":white_check_mark: 모든 명령어를 다시 불러왔습니다!")
+        else:
+            client.unload_extension(f"cogs.{extension}")
+            client.load_extension(f"cogs.{extension}")
+            await ctx.send(f":white_check_mark: {extension}을(를) 다시 불러왔습니다!")
+
+
 for filename in os.listdir("cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
-
 
  
 @client.event
@@ -46,6 +80,4 @@ async def change_status():
 
 
 
-
-access_token = os.environ["BOT_TOKEN"]
-client.run(access_token)
+client.run("Njk1NTQ2NTc3MjYzMTMyNjc0.XqTqNg.s6Qcq34Qe-9JldAHcN7b19zM38w")
